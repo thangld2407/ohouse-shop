704bb9ea8b86fb2fd246c3273e65606c42c45c36
Thang Le Dinh <thangld2407@gmail.com>
2022-05-27 17:29:00 +0700

    [FE][Add toast message, validation]

--- client/src/views/login/index.vue
+++ client/src/views/login/index.vue
@@ -4,29 +4,55 @@
       <div class="login-form__header">
         <span>Create Account</span>
         <div class="login-form__header-types-login">
-          <button class="btn btn-outline-secondary">
+          <button class="btn btn-manual-outline">
             <i class="fab fa-google"></i>
           </button>
-          <button class="btn btn-outline-secondary">
+          <button class="btn btn-manual-outline">
             <i class="fab fa-facebook"></i>
           </button>
-          <button class="btn btn-outline-secondary">
+          <button class="btn btn-manual-outline">
             <i class="fab fa-twitter"></i>
           </button>
         </div>
       </div>
       <div class="login-form__body">
-        <b-form-input v-model="fullname" placeholder="Name"></b-form-input>
-        <b-form-input v-model="email" placeholder="Email"></b-form-input>
-        <b-form-input v-model="password" placeholder="Password"></b-form-input>
+        <div class="login-form__body-form">
+          <b-form-input v-model="fullname" placeholder="Name"></b-form-input>
+          <small v-if="isErrorMessage" class="float-left text-danger">{{
+            messgaeErrors
+          }}</small>
+        </div>
+        <div class="login-form__body-form">
+          <b-form-input v-model="email" placeholder="Email"></b-form-input>
+          <small v-if="isErrorMessage" class="float-left text-danger">{{
+            messgaeErrors
+          }}</small>
+        </div>
+        <div class="login-form__body-form">
+          <b-form-input
+            type="password"
+            v-model="password"
+            placeholder="Password"
+          ></b-form-input>
+          <small v-if="isErrorMessage" class="float-left text-danger">{{
+            messgaeErrors
+          }}</small>
+        </div>
       </div>
       <div class="login-form__footer">
-        <b-form-checkbox id="checkbox-1" v-model="status" name="checkbox-1">
+        <b-form-checkbox id="checkbox-1" name="checkbox-1">
           I accept the terms and use
         </b-form-checkbox>
         <div class="login-form__footer-action">
-          <button class="btn btn-danger">Sign Up</button>
-          <button class="btn btn-outline-danger">Sign In</button>
+          <button
+            class="btn btn-manual"
+            :disabled="isProcessing"
+            @click="handleLogin()"
+          >
+            Sign Up
+            <b-spinner small v-if="isProcessing"></b-spinner>
+          </button>
+          <button class="btn btn-manual-outline">Sign In</button>
         </div>
       </div>
     </div>
@@ -34,26 +60,59 @@
 </template>
 
 <script>
-import { getProduct } from "../../api/module/product";
-import { formatPrice } from "../../utils/formatPrice";
+import { MakeToast } from "../../utils/MakeToast";
 export default {
   name: "Login",
   data() {
     return {
-      fullname: "username",
-      products: [],
-      formatPrice,
+      fullname: "",
+      email: "",
+      password: "",
+      isProcessing: false,
+      countErrors: 0,
+      messgaeErrors: "",
+      isErrorMessage: false,
     };
   },
   methods: {
-    async handleGetProduct() {
-      try {
-        const response = await getProduct();
-        this.products = response.data;
-        console.log(this.products);
-      } catch (error) {
-        console.error(error);
+    async handleLogin() {
+      this.isProcessing = true;
+      let data = {
+        email: this.email,
+        password: this.password,
+        fullname: this.fullname,
+      };
+      await this.validation(data);
+      if (this.countErrors === Object.keys(data).length) {
+        MakeToast({
+          variant: "success",
+          title: "Success",
+          content: data.email,
+        });
+        setTimeout(() => {
+        this.isErrorMessage = false;
+          this.isProcessing = false;
+        }, 2000);
       }
+    },
+    validation(data) {
+      for (let obj in data) {
+        if (data[obj] === "") {
+          this.messgaeErrors = `You can not empty field ${obj}`;
+          this.isErrorMessage = true;
+          setTimeout(() => {
+            this.isProcessing = false;
+          }, 2000);
+        } else {
+          this.countErrors += 1;
+
+        }
+      }
+    },
+    resetData() {
+      this.email = "";
+      this.password = "";
+      this.fullname = "";
     },
   },
 };
