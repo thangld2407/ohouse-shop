66e9c9c333b83953ee16746760f17edfb842975a
Winx <thangld2407@gmail.com>
2022-07-04 11:54:28 +0700

    [FE][init atomic design button]

---
+++ client/src/components/Buttons/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="v-button">
+    <button :class="styles">{{ text }} - {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueButton",
+  props: {
+    text: {
+      type: String,
+    },
+    styles: {
+      type: String,
+      default: "btn btn-primary",
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
